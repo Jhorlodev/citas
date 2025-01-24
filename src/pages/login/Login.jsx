1b55ce21b515887039a8ab43253b97608b0f4a21
jhorman <131390416+Jhorlodev@users.noreply.github.com>
2025-01-24 00:50:25 -0300

    agreggo google provider

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { supabase } from '../../components/lib/supabase'
 import './Login.css'
-
+import './Google.css'
 function Login() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
@@ -33,9 +33,9 @@ function Login() {
 
     return (
         <div className='flex flex-col items-center justify-center h-screen bg-[#edf7f7]'>
-            <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md sm:w-80 md:w-96 mx-auto">
-                <p className="text-2xl font-bold mb-4 text-gray-800">Iniciar Sesión</p>
-                
+            <div className="bg-white p-6 rounded-lg shadow-lg   sm:w-80 md:w-[500px] md:h-[650px] justify-center mx-auto">
+                <p className="  text-center text-3xl font-extrabold mb-9  text-gray-800 ">Iniciar Sesión</p>
+
                 <form onSubmit={handleSubmit}>
                     <div className="mb-4">
                         <label htmlFor="email" className="block text-sm font-medium text-gray-700">
@@ -67,9 +67,9 @@ function Login() {
                         />
                     </div>
 
-                    <button 
-                        type="submit" 
-                        className="w-full bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 rounded-md transition-colors"
+                    <button
+                        type="submit"
+                        className="w-full mx-auto text-center  bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 rounded-md transition-colors"
                     >
                         Ingresar
                     </button>
@@ -81,23 +81,23 @@ function Login() {
                     <div className="flex-grow border-t border-gray-300"></div>
                 </div>
 
-                <div className="flex justify-center space-x-4 mb-6">
-                    <button className="p-2 rounded-full hover:bg-gray-100">
-                        <svg className="w-6 h-6 text-gray-700" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z"/>
+                <div className="flex justify-center  ">
+                    <button className="button">
+                        <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" viewBox="0 0 256 262">
+                            <path fill="#4285F4" d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"></path>
+                            <path fill="#34A853" d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"></path>
+                            <path fill="#FBBC05" d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782"></path>
+                            <path fill="#EB4335" d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"></path>
                         </svg>
+                        Continue with Google
                     </button>
-                    <button className="p-2 rounded-full hover:bg-gray-100">
-                        <svg className="w-6 h-6 text-gray-700" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M23.954 4.569c-.885.389-1.83.654-2.825.775 1.014-.611 1.794-1.574 2.163-2.723-.951.555-2.005.959-3.127 1.184-.896-.959-2.173-1.559-3.591-1.559-2.717 0-4.92 2.203-4.92 4.917 0 .39.045.765.127 1.124C7.691 8.094 4.066 6.13 1.64 3.161c-.427.722-.666 1.561-.666 2.475 0 1.71.87 3.213 2.188 4.096-.807-.026-1.566-.248-2.228-.616v.061c0 2.385 1.693 4.374 3.946 4.827-.413.111-.849.171-1.296.171-.314 0-.615-.03-.916-.086.631 1.953 2.445 3.377 4.604 3.377-1.68 1.319-3.809 2.105-6.102 2.105-.39 0-.779-.023-1.17-.067 2.189 1.394 4.768 2.209 7.557 2.209 9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63.961-.689 1.8-1.56 2.46-2.548l-.047-.02z"/>
-                        </svg>
-                    </button>
+
                 </div>
 
                 <p className="text-center text-sm text-gray-500">
                     ¿No tienes cuenta? {' '}
-                    <Link 
-                        to="/NewUser" 
+                    <Link
+                        to="/NewUser"
                         className="text-indigo-600 hover:text-indigo-700 font-medium"
                     >
                         Regístrate aquí
